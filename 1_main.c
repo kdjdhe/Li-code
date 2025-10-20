5fd34a118619768bb3611e4310d1095d34870fc1
Li super <qwertyuiop43796@outlook.com>
2025-10-20 22:20:57 +0800

    Implement temperature conversion from Celsius to Fahrenheit
    Replace 'Hello World' with temperature conversion logic.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,11 @@
-//
-//
-//
+//202510125218
+//qwertyuiop43796@outlook.com
+//李小鹏
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+int main() {
+    float sheshi,huashi;
+    scanf("%f",&sheshi);
+    huashi=sheshi*9/5+32;
+    printf("%.1f\n",huashi);
     return 0;
 }
