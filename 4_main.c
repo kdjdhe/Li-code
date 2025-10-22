587bf93b75912da58b4a0cde6d76d568e3ad6c3e
Li super <qwertyuiop43796@outlook.com>
2025-10-22 21:01:17 +0800

    4_main.c

--- 4_main.c
+++ 4_main.c
@@ -3,9 +3,9 @@
 //李小鹏
 #include <stdio.h>
 int main() {
-    int a,b,c;
-    scanf("%d %d %c",&a, &b, &c);
-    if(a + b > c && a + c > b && b + c > a)
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a)
         printf("可以组成三角形\n");
     else
         printf("不能组成三角形\n");
