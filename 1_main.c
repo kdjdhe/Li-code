3449940b4465ec5792d6ce263827d61d3b3e9603
Li super <qwertyuiop43796@outlook.com>
2025-10-27 11:42:37 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,9 +3,20 @@
 //李小鹏
 #include <stdio.h>
 int main() {
-    float sheshi,huashi;
-    scanf("%f",&sheshi);
-    huashi=sheshi*9/5+32;
-    printf("%.1f\n",huashi);
+    int in,i;
+    scanf("%d", &in);
+    for ( i = 2; i * i <=in; i++)
+    {
+        if (in % i == 0)
+        {
+            printf("密钥不安全，请重新输入\n");
+            return 0;
+        }
+        else
+        {
+            printf("密钥安全，密码设置成功\n");
+        }
+        
+    }
     return 0;
 }
