f33745832c7420e38e9a96d60a60961ff17038ea
Li super <qwertyuiop43796@outlook.com>
2025-10-31 22:16:16 +0800

    6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,23 +1,32 @@
 //202510125218
 //qwertyuiop43796@outlook.com
 //李小鹏
-#include <stdio.h>
-
-int main() {
-    int arr[5];
-    int i, j;
-    for (i = 0; i < 5; i++) {
-        while (1) {
-            if (scanf("%d", &arr[i]) == 1 && arr[i] % 2 == 0) {
-                break;  
-            } else {
-                while (getchar() != '\n');
-            }
-        }
-    }
-    for (i = 0; i < 5; i++) {
-        if (i > 0) printf(" ");
-        printf("%d", arr[i]);
-    }
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int a = 0;
+	int k = 0;
+	int arr[5] = {0};
+	while(i<5)
+	{
+		scanf("%d",&j);
+		if (j % 2 == 0)
+		{
+			arr[i] = j;
+			i++;
+		}
+		
+	}
+	for (k = 0; k < 5; k++)
+	{
+		if (a == 1)
+		{
+			printf(" ");
+		}
+		printf("%d", arr[k]);
+		a = 1;
+	}
+	return 0;
 }
