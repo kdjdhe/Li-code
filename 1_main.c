54b445f83b9fb5a4741faf66b10cfe19f1eaf37a
Li super <qwertyuiop43796@outlook.com>
2025-10-31 20:46:53 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -5,19 +5,34 @@
 int main() {
     int in,i;
     scanf("%d", &in);
-    for ( i = 2; i * i <=in; i++)
+    if (in > 50 || in < 1 )
     {
-        if (in % i == 0)
-        {
+        return 1;
+    }
+    else{
+        if (in == 1) {
             printf("密钥不安全，请重新输入\n");
             return 0;
         }
-        else
+        int is_prime = 1;
+        for ( i = 2; i * i <= in; i++)
+        {
+            if (in % i == 0)
+            {
+                is_prime = 0;
+                break;
+            }
+        }
+        if (is_prime)
         {
             printf("密钥安全，密码设置成功\n");
-            break;
         }
-        
+        else
+        {
+            printf("密钥不安全，请重新输入\n");
+        }
     }
+    
+    
     return 0;
 }
