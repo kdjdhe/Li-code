9e1d707afa84cb59ba59254eb97fcd0f3958ac72
Li super <qwertyuiop43796@outlook.com>
2025-10-31 20:48:18 +0800

    3_main.c
    Refactor main function to include key safety check and input validation.

--- 3_main.c
+++ 3_main.c
@@ -2,30 +2,28 @@
 //qwertyuiop43796@outlook.com
 //李小鹏
 #include <stdio.h>
-int main() {
-    int a,b;
-    char op; 
-    scanf("%d %d %c",&a, &b, &op);
-    switch (op){
-    case '+':
-        printf("%d", a + b);
-        break;
-    
-    case '-':
-        printf("%d", a - b);
-        break;
-    case '*':
-        printf("%d", a * b);
-        break;
-    case '/':
-        if(b != 0)
-            printf("%d", a / b);
-        else
-            printf("重修小学二年级");
-        break;
-    default:
-        printf("重修小学二年级");
+int main()
+{   int i = 1;
+    int j;
+    scanf("%d", &j);
+    while (j < 1 || j > 50)
+    {
+        return 1;
     }
-    ;
+    while (i <= 50 || i >= 1)
+    {
+        i++;
+        if (j%i == 0)
+        {
+            printf("密钥不安全，请重新输入\n");
+            break;
+        }
+        else
+        {
+            printf("密钥安全，密码设置成功\n");
+            break;
+        }
+        
+    }
     return 0;
 }
