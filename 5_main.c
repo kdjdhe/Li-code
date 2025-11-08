0fde55536269010789b03727ebe1de9509623c26
Li super <qwertyuiop43796@outlook.com>
2025-11-08 17:12:15 +0800

    5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,14 +1,20 @@
 //202510125218
 //qwertyuiop43796@outlook.com
 //李小鹏
-#include <stdio.h>
-int sum(int a1,int an,int step){
-    int n =(an - a1 )/step + 1;
-    int temp = n * (a1 + an ) / 2;
-    return temp ;
+#include<stdio.h>
+int sum(int a1, int an,int step)
+{
+	int i = 0;
+	int result = 0;
+	for (i = a1; i <= an; i = i + step)
+	{
+		result += i;
+	}
+	return result;
 }
-int main(){
-    int  result = sum(1，100,1);
-    printf("%d",result);
-    return 0 ;
+int main()
+{
+	int a = sum(1,100,1);
+	printf("%d", a);
+	return 0;
 }
