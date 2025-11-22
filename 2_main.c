e76929003050dad46d2b8a4e0325eb95d54b4ef7
Li super <qwertyuiop43796@outlook.com>
2025-11-22 12:46:48 +0800

    2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,33 +2,17 @@
 //qwertyuiop43796@outlook.com
 //李小鹏
 #include <stdio.h>
+
 int main() {
-    int a, b, sum;
-    int mtx[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &mtx[i][j]);
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            
-            if (j < 2)
-            {
-                printf("%d ", mtx[j][i]);
-            }
-            else
-            {
-                printf("%d", mtx[j][i]);
-            }
-        }
-        printf("\n");
-    }
-    
-    
-    return 0;
+   int arr[5];
+   for(int i=0; i<5; i++) {
+       scanf("%d", arr + i);
+   }
+   for(int i=0; i<5; i++) {
+       *(arr + i) *= 2;
+   }
+   for(int i=0; i<5; i++) {
+       printf("%d ", *(arr + i));
+   }
+   return 0;
 }
