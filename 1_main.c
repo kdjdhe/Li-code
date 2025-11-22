f4fd8da76735160da1b99bc0dcd18be744cfbf70
Li super <qwertyuiop43796@outlook.com>
2025-11-22 12:46:17 +0800

    1_main.c
    Removed matrix input/output code and added pointer manipulation.

--- 1_main.c
+++ 1_main.c
@@ -3,32 +3,10 @@
 //李小鹏
 #include <stdio.h>
 int main() {
-    int a, b, sum;
-    int mtx[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &mtx[i][j]);
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            
-            if (j < 2)
-            {
-                printf("%d ", mtx[i][j]);
-            }
-            else
-            {
-                printf("%d", mtx[i][j]);
-            }
-        }
-        printf("\n");
-    }
-    
-    
+    int a;
+    scanf("%d", &a);
+    int *p = &a;
+    *p += 10;
+    printf("%d,%d\n", a, *p);
     return 0;
 }
