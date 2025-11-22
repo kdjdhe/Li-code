77fbda245b792b4b6e5a48ab735034894e407c5d
Li super <qwertyuiop43796@outlook.com>
2025-11-22 12:47:10 +0800

    3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,26 +2,15 @@
 //qwertyuiop43796@outlook.com
 //李小鹏
 #include <stdio.h>
-void bubble_sort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+void swap(int *a, int *b) {
+   int temp = *a;
+   *a = *b;
+   *b = temp;
 }
 int main() {
-    int arr[10];
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubble_sort(arr, n);
-    for (int i=0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    return 0;
+   int x, y;
+   scanf("%d %d", &x, &y);
+   swap(&x, &y);
+   printf("%d %d\n", x, y);
+   return 0;
 }
