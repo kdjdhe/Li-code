0c02520a83607408b1b6aa96b8c9f71dfd3a3df7
Li super <qwertyuiop43796@outlook.com>
2025-11-22 12:47:41 +0800

    4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,20 +2,19 @@
 //qwertyuiop43796@outlook.com
 //李小鹏
 #include <stdio.h>
-long long power(int a, int b) {
-    long long result = 1;
-    for (int i = 0; i < b; i++) {
-        result *= a;
-    }
-    return result;
+void incrementArray(int *arr, int size) {
+   for(int i = 0; i < size; i++) {
+       (*(arr + i))++;
+   }
 }
-
 int main() {
-    long long sum = 0;
-    for (int i = 1; i <= 5; i++) {
-        sum += power(i, 2);
-    }
-    printf("%d\n", sum); 
-    return 0;
+   int arr[5];
+   for(int i = 0; i < 5; i++) {
+       scanf("%d", &arr[i]);
+   }
+   incrementArray(arr, 5);
+   for(int i = 0; i < 5; i++) {
+       printf("%d ", arr[i]);
+   }
+   return 0;
 }
-
